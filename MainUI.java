d70bda0db721a1041e344b47834879f0d187af24
Marckytos <114682944+Marckytos@users.noreply.github.com>
2023-05-03 19:35:06 -0600

    Add files via upload

---
+++ MainUI.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.UI.Principal;
+
+public class MainUI {
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+
+}
